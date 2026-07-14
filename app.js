722738e1edb163fb9ae943ac612ab66b8adcd67a
dinukachamod17 <dclakshan17@gmail.com>
2026-07-14 21:06:40 +0530

    Implement Customer class and instantiate customer objects

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-console.log("jii");
+class Customer{
+    name;
+    age;
+    address;
 
+
+    constructor(name,age,address){
+        this.name=name;
+        this.age=age;
+        this.address=address;
+    }
+    
+}
+
+let customer1 = new Customer("dinuka",20,"gampaha");
+console.log(customer1);
+
+
+
+let customer2 = {
+    name: "chamod",
+    age: 20,
+    adress: "colombo"
+
+}
+
+console.log(customer2);
